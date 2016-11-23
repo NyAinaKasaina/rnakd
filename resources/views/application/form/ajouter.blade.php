4a7033b4afcb378148de2394408ab2d6063b4aa2
NyAinaKasaina <nyainakasaina@gmail.com>
2016-11-23 16:02:53 +0300

    refresh

--- resources/views/application/form/ajouter.blade.php
+++ resources/views/application/form/ajouter.blade.php
@@ -6,7 +6,7 @@
 <body>
 	<h1>Nouvelle App</h1>
         <hr>
-        <form method="POST" action="">
+        <form method="POST" action="/application">
             {{ csrf_field() }}
             <input type="hidden" name="_method" value="PUT"/>
             <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
@@ -14,18 +14,18 @@
             <br>
             <label for="nom">Nom de l'application:</label>
             <br>
-            <input type="text" id="nom" name="nom" placeholder="Nom de l'application" size="30" required=""/> 
- 
+            <input type="text" id="nom" name="nom" placeholder="Nom de l'application" size="30" required=""/>
+
             <br>
             <label for="description">Bref description:</label>
             <br>
-            <input type="text" id="description" name="description" placeholder="Bref description" size="30" required=""/> 
-            
+            <input type="text" id="description" name="description" placeholder="Bref description" size="30" required=""/>
+
             <br>
             <label for="details">Description plus detaillée:</label>
             <br>
             <textarea type="text" id="details" name="details" placeholder="Details de l'application" size="30" required=""></textarea>
-            
+
             <br>
             <label for="type">Type de l'application:</label>
             <br>
@@ -35,12 +35,12 @@
                 <option value="2">Type 2</option>
                 <option value="3">Type 3</option>
             </select>
-            
+
             <br>
             <label for="date">Date de création</label>
             <br>
             <input type="date" id="date" name="date" size="30" placeholder="Date de creation" required=""/>
-            
+
             <br>
             <label for="garant">Nom du garant:</label>
             <br>
@@ -52,7 +52,7 @@
             <input type="email" id="contact" name="contact" placeholder="moi@example.com" size="30" required=""/>
             <br>
             <br>
-            
+
             <button type="submit">Valider</button>
             <button type="reset">Annuler</button>
         </form>
--- resources/views/domaine/form/ajouter.blade.php
+++ resources/views/domaine/form/ajouter.blade.php
@@ -8,14 +8,14 @@
         <hr>
         <form method="POST" action="/domaine">
             {{ csrf_field() }}
-            <input type="hidden" name="_method" value="PUT"/>
+            
             <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
 
             <br>
             <label for="domaine">Domaine:</label>
             <br>
-            <input type="text" id="domaine" name="domaine" placeholder="Domaine" size="30" required=""/> 
-            
+            <input type="text" id="domaine" name="domaine" placeholder="Domaine" size="30" required=""/>
+
             <button type="submit">Valider</button>
             <button type="reset">Annuler</button>
         </form>
