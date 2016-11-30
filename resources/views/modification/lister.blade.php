9a370f47ff7918230320736e759df9c6132bc548
Mamy RAOBY <mamyraoby@outlook.com>
2016-11-30 11:01:48 +0300

    Modif show

--- resources/views/modification/lister.blade.php
+++ resources/views/modification/lister.blade.php
@@ -1,10 +1,1 @@
-<table class="table bordered">
-    <thead>
-        <tr>
-            <th>Date de modification</th>
-            <th>Nom du Dev</th>
-            <th>Version</th>
-            <th>Motif de modification</th>
-        </tr>
-    </thead>
-</table>
+<?php print_r($modifications);
