3b14ac254c398d841ff71b57b7361f51d9564bbc
Mamy RAOBY <mamyraoby@outlook.com>
2016-12-05 15:40:00 +0300

    Commentaire

--- resources/views/application/lister.blade.php
+++ resources/views/application/lister.blade.php
@@ -3,6 +3,7 @@
     <td>{{ $application['id'] }}</td>
     <td>{{ $application['nom'] }}</td>
     <td>{{ $application['domaine'] }}</td>
+    <td>{{ $application['types'] }}</td>
     <td>{{ $application['description'] }}</td>
     <td>{{ $application['date_de_modification'] }}</td>
     <td>{{ $application['version'] }}</td>
