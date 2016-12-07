8e79161358c51bda62e80b6bde61be6b2ab2ea1e
NyAinaKasaina <nyainakasaina@gmail.com>
2016-12-07 15:40:46 +0300

    sary

--- resources/views/modification/ajouter.blade.php
+++ resources/views/modification/ajouter.blade.php
@@ -1,40 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Mis à jour des informations</title>
-</head>
-<body>
-<!--
-        'degre'                  => 'required' ,
-        'date_de_modification'   => 'required' ,
-        'motif'                  => 'required' ,
-        'mailDeveloppeur_PG'     => 'required' ,
-        'application_id'         => 'required'
--->
-        <h1>Modification</h1>
-        <hr>
-        <form method="POST" action="/modification">
-            {{ csrf_field() }}
 
-            <input type="hidden" name="application_id" value="4"/>
+<h1>Modification</h1>
+<hr>
+<form method="POST" action="/modification">
+    {{ csrf_field() }}
 
-            <input type="date" name="date_de_modification" />
+    <input type="hidden" name="application_id" value="{{ $application_id }}"/>
 
-            <textarea name="motif">
-            Motif
-            </textarea>
+    <input type="date" name="date_de_modification" />
 
-            <select name="degre" required="">
-                <option value=""> --- Degré ---</option>
-                <option value="mineur"> --- Mineur ---</option>
-                <option value="majeur"> --- Majeur ---</option>
-                <option value="stabilite"> --- Stabilité ---</option>
-            </select>
+    <textarea name="motif">
+    Motif
+    </textarea>
 
-            <input type="email" name="mailDeveloppeur_PG"/>
+    <select name="degre" required="">
+        <option value=""> --- Degré ---</option>
+        <option value="mineur"> --- Mineur ---</option>
+        <option value="majeur"> --- Majeur ---</option>
+        <option value="stabilite"> --- Stabilité ---</option>
+    </select>
 
-            <button type="submit">Valider</button>
-            <button type="reset">Annuler</button>
-        </form>
-</body>
-</html>
+    <input type="email" name="mailDeveloppeur_PG"/>
+
+    <button type="submit">Valider</button>
+    <button type="reset">Annuler</button>
+</form>
